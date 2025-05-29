9d6bafb6ee9e6b52134c2e8f41ecd52f5244a371
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-29 11:09:12 +0000

    Fixed: paths to source files.

--- run-coverage.sh
+++ run-coverage.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 conan build . --build=missing --settings=build_type=Debug
 cd build/Debug
-gcovr --root=../../.. --exclude='.*test/.*' --cobertura=coverage.xml
+gcovr --root=../.. --exclude='.*test/.*' --cobertura=coverage.xml
 
 # We also need the output to stdout so GitLab can pick up the overall coverage percentage.
-gcovr --root=../../.. --exclude='.*test/.*'
+gcovr --root=../.. --exclude='.*test/.*'
