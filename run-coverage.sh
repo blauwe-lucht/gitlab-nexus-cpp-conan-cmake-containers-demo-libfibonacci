a3c8cbf137fe6a5f362fa76559e75467bf62d230
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-29 11:25:09 +0000

    Added: html coverage report.

--- run-coverage.sh
+++ run-coverage.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 
 conan build . --build=missing --settings=build_type=Debug
 cd build/Debug
+
+# This is needed to get coverage from merge requests:
 gcovr --root=../.. --exclude='.*test/.*' --cobertura=coverage.xml
 
-# We also need the output to stdout so GitLab can pick up the overall coverage percentage.
+# This is needed to be able to view a nice coverage report:
+gcovr --root=../.. --exclude='.*test/.*' --html=coverage.html
+
+# We also need the output to stdout so GitLab can pick up the overall coverage percentage:
 gcovr --root=../.. --exclude='.*test/.*'
