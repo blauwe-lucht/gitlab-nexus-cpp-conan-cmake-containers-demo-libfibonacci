08b31aab3bf281d7403dd670390aea8363f1e899
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-29 11:01:08 +0000

    Added: code coverage percentage to result of pipeline.

--- run-coverage.sh
+++ run-coverage.sh
@@ -5,3 +5,6 @@ set -euo pipefail
 conan build . --build=missing --settings=build_type=Debug
 cd build/Debug
 gcovr --root=../../.. --exclude='.*test/.*' --cobertura=coverage.xml
+
+# We also need the output to stdout so GitLab can pick up the overall coverage percentage.
+gcovr --root=../../.. --exclude='.*test/.*'
