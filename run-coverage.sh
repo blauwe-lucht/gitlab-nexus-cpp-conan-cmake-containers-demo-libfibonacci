4bd971d19049354f462a33669a6a189cb3641ba4
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-30 05:42:45 +0000

    Changed: simplified run-coverage.sh.

--- run-coverage.sh
+++ run-coverage.sh
@@ -6,10 +6,7 @@ conan build . --build=missing --settings=build_type=Debug
 cd build/Debug
 
 # This is needed to get coverage from merge requests:
-gcovr --root=../.. --exclude='.*test/.*' --cobertura=coverage.xml
+gcovr --root ../.. --xml-pretty --exclude='.*test/.*' --exclude-unreachable-branches --print-summary --cobertura=coverage.xml
 
 # This is needed to be able to view a nice coverage report:
-gcovr --root=../.. --exclude='.*test/.*' --html-details=coverage.html
-
-# We also need the output to stdout so GitLab can pick up the overall coverage percentage:
-gcovr --root=../.. --exclude='.*test/.*'
+gcovr --root ../.. --exclude='.*test/.*' --exclude-unreachable-branches --html-nested=coverage.html --html-self-contained
